ec1b9da0b93b6b4d43e5a0e32a727d77e16b6b85
Leandro Borges <leandrinho_216@hotmail.com>
2025-10-03 21:38:30 -0300

    Iniciando base do projeto de autenticação login/register

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render('home.ejs');
+});
+
+app.get('/login', (req, res) => {
+	res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+	res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+	console.log(username);
+	console.log(password);
+});
+
+app.post('/login', async (req, res) => {
+	console.log(username);
+	console.log(password);
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
